7479df06aae55a7ee1d626d82ea3725f261d2fe3
Egill Hreinsson <egillh210@gmail.com>
2025-03-06 19:26:26 +0000

    update start blocks in config file

--- src/ContangoProxy.ts
+++ src/ContangoProxy.ts
@@ -17,7 +17,7 @@ import { getIMoneyMarketEventsStartBlock } from "./utils/constants";
 import { contangoAbi } from "./abis";
 import { clients } from "./clients";
 
-// re-run indexing
+// re-run indexing again again
 ContangoProxy.PositionUpserted.handler(async ({ event, context }) => {
   const contangoEvent: PositionUpsertedEvent = {
     ...event.params,
